3642dc70e0ed7b28271770ff0b728f2fe31b153f
ebacala <e.bacala@gmail.com>
2017-12-12 10:35:10 +0100

    Let'go !

--- LanguageModel/src/langModel/Vocabulary.java
+++ LanguageModel/src/langModel/Vocabulary.java
@@ -1,75 +1,79 @@
 package langModel;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
 /**
  * Class Vocabulary: class implementing the interface VocabularyInterface.
- * 
- * @author ... (2017)
  *
+ * @author Evann BACALA (2017)
  */
 public class Vocabulary implements VocabularyInterface {
-	/**
-	 * The set of words corresponding to the vocabulary.
-	 */
-	protected Set<String> vocabulary;
+    /**
+     * The set of words corresponding to the vocabulary.
+     */
+    protected Set<String> vocabulary;
 
-	
-	/**
-	 * Constructor.
-	 */
-	public Vocabulary(){
-		//TODO
-	}
-	
-	
-	@Override
-	public int getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	@Override
-	public Set<String> getWords() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Constructor.
+     */
+    public Vocabulary() {
+        vocabulary = new HashSet<String>();
+    }
 
-	@Override
-	public boolean contains(String word) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	@Override
-	public void addWord(String word) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * @return The size of the vocabulary Set
+     */
+    @Override
+    public int getSize() {
+        return this.vocabulary.size();
+    }
 
-	@Override
-	public void removeWord(String word) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * @return The vocabulary Set
+     */
+    @Override
+    public Set<String> getWords() {
+       return  this.vocabulary;
+    }
 
-	@Override
-	public void scanNgramSet(Set<String> ngramSet) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public boolean contains(String word) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void readVocabularyFile(String filePath) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void addWord(String word) {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void writeVocabularyFile(String filePath) {
-		// TODO Auto-generated method stub
-		
-	}
+    }
+
+    @Override
+    public void removeWord(String word) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void scanNgramSet(Set<String> ngramSet) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void readVocabularyFile(String filePath) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void writeVocabularyFile(String filePath) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
