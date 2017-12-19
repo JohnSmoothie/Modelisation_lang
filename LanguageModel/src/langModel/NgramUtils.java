cedcb081008fc3451163de2169c59ef6740009ba
ebacala <e.bacala@gmail.com>
2017-12-19 10:46:39 +0100

    boom

--- LanguageModel/src/langModel/NgramUtils.java
+++ LanguageModel/src/langModel/NgramUtils.java
@@ -1,143 +1,147 @@
 package langModel;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-
+import java.util.*;
 
 
 /**
  * Class NgramUtils: class containing useful functions to deal with n-grams.
- * 
- * @author N. Hernandez and S. Quiniou (2017)
  *
+ * @author Evann BACALA, Victor BOIX
  */
 public class NgramUtils {
 
-	/**
-	 * Method counting the number of words in a given sequence 
-	 * (the sequence can be a n-gram or a sentence).
-	 * 
-	 * @param sequence the sequence to consider.
-	 * @return the number of words of the given sequence.
-	 */
-	public static int getSequenceSize (String sequence) {
-		Scanner sc = new Scanner(sequence);
-		sc.useDelimiter(" ");
-		int cmp = 0;
-		while(sc.hasNext()) {
-			cmp++;
-			sc.next();
-		}
-		return cmp;
-	}
-
-	
-	/**
-	 * Method parsing a n-gram and returning its history, i.e. the n-1 preceding words.
-	 * 
-	 * Example: 
-	 *   let the ngram be "l' historique de cette phrase";
-	 *   the history will be given for the last word of the ngram, here "phrase":
-	 *   if the order is 2 then the history will be "cette"; 
-	 *   if the order is 3 then it will be "de cette".
-	 * 
-	 * @param ngram the n-gram to consider.
-	 * @param order the order to consider for the n-gram.
-	 * @return history of the given n-gram (the length of the history is order-1).  
-	 */
-	public static String getHistory (String ngram, int order) {
-		String res = "";
-		if(order >= 1) {
-			String[] wArray = ngram.split(" ");
-			List<String> wordList = Arrays.asList(wArray);
-			
-			int taille = wordList.size();
-			String lastWord = wordList.get(taille);
-			if(order == 1) {
-				res = lastWord;
-			} else {
-				for(int i = taille-1; i>taille; i--) {
-					res += wordList.get(i);
-				}
-			}
-		}
-		return res;
-	}
+    /**
+     * Method counting the number of words in a given sequence
+     * (the sequence can be a n-gram or a sentence).
+     *
+     * @param sequence the sequence to consider.
+     *
+     * @return the number of words of the given sequence.
+     */
+    public static int getSequenceSize(String sequence) {
+        if (sequence.trim().equals("")) return 0;
+        return sequence.split("\\s+").length;
+    }
 
 
-	/**
-	 * Method decomposing the given sentence into n-grams of the given order.
-	 * 
-	 * This method will be used in the LanguageModelInterface class for computing 
-	 * the probability of a sentence as the product of the probabilities of its n-grams. 
-	 * 
-	 * Example
-	 * given the sentence "a b c d e f g", with order=3,
-	 * it will result in the following list:
-	 * [a, a b, a b c, b c d, c d e, d e f, e f g] 
-	 * 
-	 * @param sentence the sentence to consider.
-	 * @param order the maximal order for the n-grams to create from the sentence.
-	 * @return the list of n-grams constructed from the sentence.
-	 */
-	public static List<String> decomposeIntoNgrams (String sentence, int order) {
-		List<String> list = new ArrayList<String>();
-		String[] wArray = sentence.split(" ");
-		List<String> listTemp = Arrays.asList(wArray);
-		
-		
-		
-		
-		return list;
-	}
-	
-	
-	/**
-	 * Method parsing the given sentence and generate all the combinations of ngrams,
-	 * by varying the order n between the given minOrder and maxOrder.
-	 * 
-	 * This method will be used in the NgramCount class for counting the ngrams 
-	 * occurring in a corpus.
-	 * 
-	 * Algorithm (one possible algo...)
-	 * initialize list of ngrams
-	 * for n = minOrder to maxOrder (for each order)
-	 * 	 for i = 0 to sentence.length-n (parse the whole sentence)
-	 *     initialize ngram string parsedSentence
-	 *     for j = i to i+n-1 (create a ngram made of the following sequence of words starting from i to i + the order size)
-	 *       ngram = ngram + " " + sentence[j]
-	 *     add ngramm to list ngrams 
-	 * return list ngrams
-	 * 
-	 * Example
-	 * given the sentence "a b c d e f g", with minOrder=1 and maxOrder=3, it will result in the following list:
-	 * [a, b, c, d, e, f, g, a b, b c, c d, d e, e f, f g, a b c, b c d, c d e, d e f, e f g]
-	 * 
-	 * @param sentence the sentence from which to generate n-grams.
-	 * @param minOrder the minimal order of the n-grams to create.
-	 * @param maxOrder the maximal order of the n-grams to create.
-	 * @return a list of generated n-grams from the sentence.
-	 */
-	public static List<String> generateNgrams (String sentence, int minOrder, int maxOrder) {
-		//TODO
-		return null;
-	}
-	
-	/**
-	 * Method parsing a sequence of words and returning the modified string where
-	 * out-of-vocabulary words are replaced with the OOV tag.
-	 * 
-	 * @param s the string to consider.
-	 * @param vocab the vocabulary to consider.
-	 * @return the sequence of words with OOV tags according to the vocabulary. 
-	 */
-	public static String getStringOOV(String s, VocabularyInterface vocab) {
-		//TODO
-		return "";
-	}
+    /**
+     * Method parsing a n-gram and returning its history, i.e. the n-1 preceding words.
+     * <p>
+     * Example:
+     * let the ngram be "l' historique de cette phrase";
+     * the history will be given for the last word of the ngram, here "phrase":
+     * if the order is 2 then the history will be "cette";
+     * if the order is 3 then it will be "de cette".
+     *
+     * @param ngram the n-gram to consider.
+     * @param order the order to consider for the n-gram.
+     *
+     * @return history of the given n-gram (the length of the history is order-1).
+     */
+    public static String getHistory(String ngram, int order) {
+        StringJoiner history = new StringJoiner(" ");
+        String[] words = ngram.split("\\s+");
+
+        for(int i = words.length - order; i > words.length && i >= 0; i++) {
+            history.add(words[i]);
+        }
+
+        return history.toString();
+    }
+
+
+    /**
+     * Method decomposing the given sentence into n-grams of the given order.
+     * <p>
+     * This method will be used in the LanguageModelInterface class for computing
+     * the probability of a sentence as the product of the probabilities of its n-grams.
+     * <p>
+     * Example
+     * given the sentence "a b c d e f g", with order=3,
+     * it will result in the following list:
+     * [a, a b, a b c, b c d, c d e, d e f, e f g]
+     *
+     * @param sentence the sentence to consider.
+     * @param order    the maximal order for the n-grams to create from the sentence.
+     *
+     * @return the list of n-grams constructed from the sentence.
+     */
+    public static List<String> decomposeIntoNgrams(String sentence, int order) {
+        /*List<String> ngramsList = new ArrayList<String>();
+        List<String> wordList = Arrays.asList(sentence.split(" "));
+        StringJoiner ngram = new StringJoiner(" ");
+
+        if (order == 1) {
+                ngramsList.addAll(wordList);
+        } else {
+            for (int i = wordList.size() - 1; i >= 0; i--) {
+                if (i - order >= 0) {
+                    ngramsList.add(getHistory(sentence.substring(0, i), order));
+                } else {
+                    order--;
+                }
+            }
+        }
+        return ngramsList;*/
+        List<String> ngrams = new ArrayList();
+        String[] words = sentence.split("\\s+");
+        StringBuffer parsedSentence = new StringBuffer();
+
+        for(int i = 0; i < words.length; ++i) {
+            parsedSentence.append(" ");
+            parsedSentence.append(words[i]);
+            ngrams.add((getHistory(parsedSentence.toString(), order) + " " + words[i]).trim());
+        }
+
+        return ngrams;
+    }
+
+
+    /**
+     * Method parsing the given sentence and generate all the combinations of ngrams,
+     * by varying the order n between the given minOrder and maxOrder.
+     * <p>
+     * This method will be used in the NgramCount class for counting the ngrams
+     * occurring in a corpus.
+     * <p>
+     * Algorithm (one possible algo...)
+     * initialize list of ngrams
+     * for n = minOrder to maxOrder (for each order)
+     * for i = 0 to sentence.length-n (parse the whole sentence)
+     * initialize ngram string parsedSentence
+     * for j = i to i+n-1 (create a ngram made of the following sequence of words starting from i to i + the order size)
+     * ngram = ngram + " " + sentence[j]
+     * add ngramm to list ngrams
+     * return list ngrams
+     * <p>
+     * Example
+     * given the sentence "a b c d e f g", with minOrder=1 and maxOrder=3, it will result in the following list:
+     * [a, b, c, d, e, f, g, a b, b c, c d, d e, e f, f g, a b c, b c d, c d e, d e f, e f g]
+     *
+     * @param sentence the sentence from which to generate n-grams.
+     * @param minOrder the minimal order of the n-grams to create.
+     * @param maxOrder the maximal order of the n-grams to create.
+     *
+     * @return a list of generated n-grams from the sentence.
+     */
+    public static List<String> generateNgrams(String sentence, int minOrder, int maxOrder) {
+        //TODO
+        return null;
+    }
+
+    /**
+     * Method parsing a sequence of words and returning the modified string where
+     * out-of-vocabulary words are replaced with the OOV tag.
+     *
+     * @param s     the string to consider.
+     * @param vocab the vocabulary to consider.
+     *
+     * @return the sequence of words with OOV tags according to the vocabulary.
+     */
+    public static String getStringOOV(String s, VocabularyInterface vocab) {
+        //TODO
+        return "";
+    }
 
 }
